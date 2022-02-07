f33ce0bc02a91374ccecc4bb7193973c06edfdc3
darkflamemaster95 <65520764+darkflamemaster95@users.noreply.github.com>
2022-02-07 09:51:08 +0800

    Add files via upload

---
+++ upgrade_driver.py
@@ -0,0 +1,466 @@
+import os 
+import sys
+import math
+import time
+import signal
+import struct
+import threading
+
+from tqdm import trange
+from ethernet_provider import Ethernet_Dev
+from sdk_boot import XLDR_TESEO5_BOOTLOADER_CUT2, BLOCK_SIZE, CRC32_TAB
+
+CMD_start = [0x55, 0x55]
+
+CMD_list = {
+    'PG': b'\x01\xcc',
+    'OR': b'\x0b\xcc', # [0x0b, 0xcc] 
+    'JI': b'\x01\xaa', # [0x01, 0xaa]
+    'JA': b'\x02\xaa', # [0x02, 0xaa]
+    'WA': b'\x03\xaa', # [0x03, 0xaa]
+    'CS': b'\x04\xaa', # [0x04, 0xaa]
+    'JS': b'\x05\xaa', # [0x05, 0xaa]
+    'JG': b'\x06\xaa', # [0x06, 0xaa]
+    'WS': b'\x07\xaa', # [0x07, 0xaa]
+    'WP': b'\x08\xaa', # [0x08, 0xaa]
+    'IMU_JI': b'\x4a\x49', # [0x49, 0x4a]
+    'IMU_JA': b'\x4a\x41', # [0x41, 0x4a]
+    '0a09': b'\x09\x0a',
+    'IMU_WA': b'\x57\x41'} # [0x41, 0x57]
+
+class UpgradeDriver:
+    def __init__(self):
+        self.ether = Ethernet_Dev()
+    
+    def sniff_dev(self):
+        result = self.ether.find_device()
+        if result != True:
+            return
+        time.sleep(1)
+        result = self.ether.ping_device()
+        if result != True:
+            print('Ethernet ping error.')
+            return
+
+    def setup(self):
+        fw_file = open('./bin/INS401_28.01.09.bin', "rb")
+        fw_data = fw_file.read()
+        # content_len = len(fw_data)
+        return fw_data
+
+    def get_dev_info(self):
+        command = CMD_list['OR']
+        message_bytes = []
+        self.ether.send_msg(command, message_bytes)
+        time.sleep(0.5)
+        try:
+            result = self.ether.write_read_response(command, message=[])
+            data = result[2]
+            # return data
+        except Exception as e:
+            print(e)
+            raise
+
+    # RTK/INS upgrade protocol
+    def shake_hand(self, waiting_time=5, retry_times=1):
+        time.sleep(waiting_time)
+        for i in range(retry_times):
+            result = self.ether.reshake_hand()
+            if result == True:
+                break
+            else:
+                retry_times += 1
+
+    def jump2boot(self, waiting_time):
+        command = CMD_list['JI']
+        message_bytes = []
+
+        try:
+            result = self.ether.write_read_response(command, message_bytes)
+        except Exception as e:
+            print(e)
+            raise
+
+        if result[0] != []:
+            pass
+        else:
+            print('send JI command failed')
+        time.sleep(waiting_time)
+
+    def jump2app(self, waiting_time):
+        command = CMD_list['JA']
+        message_bytes = []
+
+        try:
+            result = self.ether.write_read_response(command, message_bytes)
+        except Exception as e:
+            print(e)
+            raise
+
+        if result[0] != []:
+            pass
+        else:
+            print('send JI command failed')
+        time.sleep(waiting_time)
+
+    def before_write_content(self, core, content_len):
+        command = CMD_list['CS']
+
+        message_bytes = [ord('C'), ord(core)]
+        message_bytes.extend(struct.pack('>I', content_len))
+       
+        for _ in range(3):
+            time.sleep(1)
+            result = self.ether.write_read_response(command, message_bytes)
+            time.sleep(1)
+            if result[2] != []:
+                break
+        # data_buffer = result[2]
+        # if isinstance(data_buffer, bytes):
+        #     update_core = struct.unpack('<c', data_buffer)
+        #     if update_core == 'S':
+        #         print('Set update core and Bin size success')
+        #     elif update_core == 'F':
+        #         print('Set update core and Bin size Failed')
+        #         self.kill_app(1, 2)
+            
+        if result is None:
+            print('send cs command failed, core:{0}'.format(ord(core)))
+            self.kill_app(1, 2)
+
+    def write_block(self, num_bytes, current, upgrade_flag, data):
+        command = CMD_list['WA']
+        message_bytes = []
+        message_bytes.extend(struct.pack('>I', current))
+        message_bytes.extend(struct.pack('>I', num_bytes))
+        message_bytes.extend(data)
+        
+        try:
+            self.ether.send_msg(command, message_bytes)
+            if upgrade_flag == 0:
+                # print('Bootloader is trying to delet the flash...')
+                time.sleep(20)
+        except Exception as ex:
+            print('Write Firmware message Failed')
+
+    # IMU upgrade protocol
+    def imu_jump2boot(self, waiting_time):
+        command = CMD_list['IMU_JI']
+        message_bytes = []
+
+        try:
+            result = self.ether.write_read_response(command, message_bytes)
+        except Exception as e:
+            print(e)
+            raise
+
+        if result[0] != command:
+            print('send IMU_JI command failed')
+            self.kill_app(1, 2)
+
+        time.sleep(waiting_time)
+
+    def imu_jump2app(self, waiting_time):
+        command = CMD_list['IMU_JA']
+        message_bytes = []
+
+        try:
+            result = self.ether.write_read_response(command, message_bytes)
+        except Exception as e:
+            print(e)
+            raise
+
+        if result[0] != command:
+            print('send IMU_JA command failed')
+            self.kill_app(1, 2)
+
+        time.sleep(waiting_time)
+
+    def imu_write_block(self, data_len, current, data):
+        message_type = [0x41, 0x57] # command 'WA'
+        message_bytes = []
+        current_bytes = struct.pack('>I', current)
+        message_bytes.extend(current_bytes)
+        message_bytes.extend(struct.pack('>I', data_len))
+        message_bytes.extend(data)
+
+        packet = []
+        packet.extend(message_type)
+        msg_len = len(message_bytes)
+        packet.append(msg_len)
+        final_packet = packet + message_bytes
+        command = CMD_start + final_packet + self.ether.calc_crc(final_packet)
+
+        try:
+            self.ether.send_msg(command)
+            result = self.ether.read_until(current_bytes, message_type, 200)
+        except Exception as e:
+            print(e)
+            raise
+
+        if result == False:
+            print('send WA command failed')
+            self.kill_app(1, 2)
+
+
+
+    # 9100sdk upgrade protocol
+    def sdk_jump2boot(self, waiting_time):
+        command = CMD_list['JS']
+        message_bytes = []
+        retry_times = 5
+
+        for i in range(retry_times):
+            try:
+                result = self.ether.write_read_response(command, message_bytes)
+            except Exception as e:
+                print(e)
+                raise
+
+            if result[0] == command:
+                return
+            
+        print('JS command send failed')
+        self.kill_app(1, 2)
+
+    def sdk_jump2app(self, waiting_time):
+        command = CMD_list['JA']
+        message_bytes = []
+
+        try:
+            result = self.ether.write_read_response(command, message_bytes)
+        except Exception as e:
+            print(e)
+            raise
+        
+        if result[0] == command:
+            pass
+        else:
+            print('JA command send failed')
+
+    def sdk_sync(self):
+        command = b'\x07\xaa'
+        # command = [0x07, 0xaa]
+        sync = [0xfd, 0xc6, 0x49, 0x28]
+        # check_data = [0x3A, 0x54, 0x2C, 0xA6]
+        retry = 20
+        result = False
+
+        for _ in range(retry):
+            result = self.ether.write_read_response(command, sync, True, 2)
+            if result[1] != []:
+                break
+
+        if result[2] == bytes([0x3A, 0x54, 0x2C, 0xA6]):
+            result = True
+        else:
+            return result
+        return result
+
+        # for _ in range(retry):
+        #     for _ in range(2):
+        #         self.ether.send_packet(sync)
+        #     time.sleep(0.05)
+        #     result = self.ether.read_until(check_data, command, 30)
+        #     if result is False:
+        #         time.sleep(0.001)
+        #         continue
+        #     else:
+        #         break
+        
+        # return result
+
+    def change_buad(self):
+        command = b'\x07\xaa'
+        change_baud_cmd = [0x71]
+        
+        for i in range(3):
+            result = self.ether.write_read_response(command, change_baud_cmd)
+            time.sleep(1)
+            if result[2] == b'\xcc':
+                break
+            elif i == 2:
+                return False
+        return True
+
+    def send_baud(self, baud_int):
+        command = b'\x07\xaa'
+        baud_list = []
+        baud_list = self.ether.get_list_from_int(baud_int)
+
+        for i in range(3):
+            self.ether.send_packet(baud_list)
+            result = self.ether.write_read_response(command, baud_list)
+            if result[2] == b'\xcc':
+                break
+            elif i == 2:
+                return False
+        return True
+
+    def baud_check(self):
+        command = b'\x07\xaa'
+        check_baud = [0x38]
+        time.sleep(0.01)
+        for i in range(3):
+            result = self.ether.write_read_response(command, check_baud)
+            if result[2] == b'\xcc':
+                break
+            elif i == 2:
+                return False
+        return True
+
+    def is_host_ready(self):
+        command = b'\x07\xaa'
+        host = [0x5a]
+        
+        for i in range(3):
+            result = self.ether.write_read_response(command, host)
+            if result[2] == b'\xcc':
+                break
+            elif i == 2:
+                return False
+        return True
+
+    def send_boot(self):
+        boot_size = len(XLDR_TESEO5_BOOTLOADER_CUT2)
+        boot_size_hex = []
+        boot_size_hex = self.ether.get_list_from_int(boot_size)
+
+        crc_val_boot = 0
+        crc_val_boot = self.ether.sdk_crc(crc_val_boot, boot_size_hex, 4)
+        entry_hex = [0, 0, 0, 0]
+        crc_val_boot = self.ether.sdk_crc(crc_val_boot, entry_hex, 4)
+        crc_val_boot = self.ether.sdk_crc(
+            crc_val_boot, XLDR_TESEO5_BOOTLOADER_CUT2, boot_size)
+        #crc_val_boot_hex=[0 for x in range(0,4)]
+        crc_val_boot_hex = []
+        crc_val_boot_hex = self.ether.get_list_from_int(crc_val_boot)
+
+        boot_part1 = XLDR_TESEO5_BOOTLOADER_CUT2[0:5120]
+        boot_part2 = XLDR_TESEO5_BOOTLOADER_CUT2[5120:]
+
+        preamble = [0xf4, 0x01, 0xd5, 0xbc, 0x73, 0x40, 0x98,
+                    0x83, 0x04, 0x01, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00]
+
+        self.ether.send_packet(preamble)
+        self.ether.send_packet(crc_val_boot_hex)
+        self.ether.send_packet(boot_size_hex)
+        self.ether.send_packet(entry_hex)
+        self.ether.send_packet(boot_part1)
+        self.ether.send_packet(boot_part2)
+        
+        return True
+
+    def send_write_flash(self):
+        command = b'\x07\xaa'
+        write_cmd = [0x4A]
+
+        for i in range(3):
+            result = self.ether.write_read_response(command, write_cmd)
+            if result[2] == b'\xcc':
+                break
+            elif i == 2:
+                return False
+        return True
+
+    def get_bin_info_list(self, fs_len, bin_data):
+        bootMode = 0x01
+        destinationAddress = 0x10000000
+        entryPoint = 0
+        erase_nvm_u8 = 3
+        eraseOnly_u8 = 0
+        programOnly_u8 = 0
+        subSector_u8 = 0
+        sta8090fg_u8 = 0
+        res1_8 = 0
+        res2_8 = 0
+        res3_8 = 0
+        nvmoffset = 0
+        nvmEraseSize = 0
+        debug = 0
+        debugAction = 0
+        debugAddress = 0
+        debugSize = 0
+        debugData = 0
+
+        crc_file = 0
+        file_size_list = self.ether.get_list_from_int(fs_len)
+        crc_file = self.ether.sdk_crc(crc_file, file_size_list, 4)
+        crc_file = self.ether.sdk_crc(crc_file, bin_data, fs_len)
+
+        bin_info_list = []
+        bin_info_list += self.ether.get_list_from_int(fs_len)
+        bin_info_list += self.ether.get_list_from_int(bootMode)
+        bin_info_list += self.ether.get_list_from_int(crc_file)
+        bin_info_list += self.ether.get_list_from_int(destinationAddress)
+        bin_info_list += self.ether.get_list_from_int(entryPoint)
+        bin_info_list.append(erase_nvm_u8)
+        bin_info_list.append(eraseOnly_u8)
+        bin_info_list.append(programOnly_u8)
+
+        bin_info_list.append(subSector_u8)
+        bin_info_list.append(sta8090fg_u8)
+        bin_info_list.append(res1_8)
+        bin_info_list.append(res2_8)
+        bin_info_list.append(res3_8)
+        bin_info_list += self.ether.get_list_from_int(nvmoffset)
+        bin_info_list += self.ether.get_list_from_int(nvmEraseSize)
+        bin_info_list += self.ether.get_list_from_int(debug)
+        bin_info_list += self.ether.get_list_from_int(debugAction)
+        bin_info_list += self.ether.get_list_from_int(debugAddress)
+        bin_info_list += self.ether.get_list_from_int(debugSize)
+        bin_info_list += self.ether.get_list_from_int(debugData)
+        # print(bin_info_list)
+        return bin_info_list
+
+    def send_bin_info(self, bin_info_list):
+        for _ in range(3):
+            self.ether.send_packet(bin_info_list, buffer_size=512)
+            result = self.ether.read_until([0xCC], [0x07, 0xaa], 200)
+            time.sleep(3)
+            if result:
+                break
+        return True
+
+    def wait(self):
+        return self.ether.read_until([0xCC], [0x07, 0xaa], 500)
+
+    def flash_write_pre(self, content):
+        data_to_sdk = content[0:BLOCK_SIZE]
+        self.ether.send_packet(list(data_to_sdk), send_method=[0x08, 0xaa])
+
+    def flash_write(self, fs_len, bin_data):
+        write_result = True
+        packet_num = math.ceil(fs_len/BLOCK_SIZE)
+        current = BLOCK_SIZE
+        for i in trange(1, packet_num):
+            if i == packet_num:
+                data_to_sdk = bin_data[packet_num*BLOCK_SIZE:]
+            else:
+                data_to_sdk = bin_data[i*BLOCK_SIZE:(i+1)*BLOCK_SIZE]
+
+            current += len(data_to_sdk)
+            for i in range(3):
+                self.ether.send_packet(list(data_to_sdk))
+                has_read = self.ether.read_until([0xCC], [0x07, 0xaa], 200)
+                if has_read:
+                    break
+
+            if has_read:
+                pass
+            else:
+                write_result = False
+                break
+
+        return write_result
+
+    def flash_crc(self):
+        return self.ether.read_until([0xCC], [0x07, 0xaa], 2000, 1)
+
+    def flash_restart(self):
+        return self.ether.read_until([0xCC], [0x07, 0xaa], 200, 1)
+
+    # kill main thread 
+    def kill_app(self, signal_int, call_back):
+        os.kill(os.getpid(), signal.SIGTERM)
+        sys.exit()
---
+++ upgrade_executor.py
@@ -0,0 +1,218 @@
+import os 
+import sys
+import time 
+import struct
+import threading
+
+from tqdm import trange
+from ethernet_provider import Ethernet_Dev
+from upgrade_driver import UpgradeDriver
+
+PART_NAME = ['rtk', 'ins', 'sdk', 'imu']
+
+class Executor:
+    def __init__(self):
+        self.driver = UpgradeDriver()
+        self.ether = Ethernet_Dev()
+        self.is_stop = False
+        self.flag_list = []
+        self.fw_part_lens_list = []
+        self.fw_part_list = []
+        
+    def upgrade_work(self):
+        self.driver.sniff_dev()
+        self.driver.get_dev_info()
+        '''TODO
+        parse the response to get info
+        '''
+        content = self.driver.setup()
+        content_len = len(content)
+
+        rtk_start_flag = content.find(b'rtk_start:')
+        ins_start_flag = content.find(b'ins_start:')
+        sdk_start_flag = content.find(b'sdk_start:')
+        imu_start_flag = content.find(b'imu_start:')
+        self.flag_list = [rtk_start_flag, ins_start_flag, sdk_start_flag, imu_start_flag]
+
+        rtk_data_lens = struct.unpack('<I', content[(rtk_start_flag+10):(rtk_start_flag+14)])[0]
+        ins_data_lens = struct.unpack('<I', content[(ins_start_flag+10):(ins_start_flag+14)])[0] 
+        sdk_data_lens = struct.unpack('<I', content[(sdk_start_flag+10):(sdk_start_flag+14)])[0]
+        imu_data_lens = struct.unpack('<I', content[(imu_start_flag+10):(imu_start_flag+14)])[0]
+        self.fw_part_lens_list = [rtk_data_lens, ins_data_lens, sdk_data_lens, imu_data_lens]
+
+        rtk_bin_data = content[(rtk_start_flag+14):ins_start_flag]
+        ins_bin_data = content[(ins_start_flag+14):sdk_start_flag]
+        sdk_bin_data = content[(sdk_start_flag+14):imu_start_flag]
+        imu_bin_data = content[(imu_start_flag+14):content_len]
+        self.fw_part_list = [rtk_bin_data, ins_bin_data, sdk_bin_data, imu_bin_data]
+
+        # a = self.driver.get_bin_info_list(len(sdk_bin_data), sdk_bin_data)
+
+        pass_time = 0
+        for i in range(len(self.fw_part_list)):
+            data_lens = len(self.fw_part_list[i])
+            if data_lens == self.fw_part_lens_list[i]:
+                pass_time += 1
+            else:
+                failed_part = PART_NAME[i]
+                print(f'Firmware validity check failed, failed on {failed_part}') 
+
+        if pass_time != len(self.fw_part_list):
+            print('Upgrade Failed')
+            time.sleep(2)
+            self.driver.kill_app(1, 2)
+
+        # upgrade rtk/ins part of the device
+        print('Upgrade strat...')
+        # self.driver.jump2boot(3)
+        # self.driver.shake_hand()
+        # print('rtk part upgrade start')
+        # self.rtk_part_upgrade(self.fw_part_list[0])
+        # time.sleep(0.5)
+        # print('ins part upgrade start')
+        # self.ins_part_upgrade(self.fw_part_list[1])
+        # time.sleep(0.5)
+        # self.driver.jump2app(10)   
+
+        # # # upgrade sdk9100 part of the device
+        self.driver.shake_hand()
+        self.driver.sdk_jump2boot(3)
+        self.driver.shake_hand()
+        print('sdk part upgrade start')
+        self.sdk_part_upgrade(self.fw_part_list[2])
+        self.driver.sdk_jump2app(3)
+        print('sdk upgrade successed')
+        
+        # upgrade imu part of the device
+        self.driver.shake_hand()
+        self.driver.imu_jump2boot(3)
+        self.driver.shake_hand()
+        time.sleep(0.5)
+        print('imu part upgrade start')
+        self.imu_part_upgrade(self.fw_part_list[3])
+        time.sleep(0.5)
+        self.driver.jump2app(3)
+
+    def rtk_part_upgrade(self, content):
+        core = '0'
+        content_len = len(content)
+        upgrade_flag = 0
+        self.driver.before_write_content(core, content_len)
+
+        # self.ether.reset_buffer()
+        step = 192
+        current_side = self.flag_list[0]
+        write_turns = int(content_len/step)
+        
+        for _ in trange(write_turns):
+            target_content = content[current_side: (current_side+step)]
+            time.sleep(0.01)
+            self.driver.write_block(step, current_side, upgrade_flag, target_content)
+            time.sleep(0.01)
+            current_side += step
+            upgrade_flag += 1
+        
+        extract_content_flag = content_len - (step * write_turns)
+        extract_content = content[(content_len-extract_content_flag):content_len]
+        upgrade_flag += 1
+
+        self.driver.write_block(extract_content_flag, current_side, upgrade_flag, extract_content)
+        print('rtk upgrade successed\n')
+
+    def ins_part_upgrade(self, content):
+        core = '1'
+        content_len = len(content)
+        upgrade_flag = 0
+        self.driver.before_write_content(core, content_len)
+
+        step = 192
+        copy_side = 0 # just a fake side to cut content
+        current_side = self.flag_list[1] # current_side is an actual side of bin and it will correspond to copy_side
+        write_turns = int(content_len/step)
+        
+        for _ in trange(write_turns):
+            target_content = content[copy_side: (copy_side+step)]
+            time.sleep(0.01)
+            self.driver.write_block(step, copy_side, upgrade_flag, target_content)
+            time.sleep(0.01)
+            copy_side += step
+            current_side += step
+            upgrade_flag += 1
+        
+        extract_content_flag = content_len - (step * write_turns)
+        extract_content = content[copy_side:(copy_side+extract_content_flag)]
+        upgrade_flag += 1
+
+        self.driver.write_block(extract_content_flag, copy_side, upgrade_flag, extract_content)
+        print('ins upgrade successed\n')
+
+    def imu_part_upgrade(self, content):
+        content_len = len(content)
+        step = 192
+        copy_side = 0 # just a fake side to cut content
+        current_side = self.flag_list[3] # current_side is an actual side of bin and it will correspond to copy_side
+        write_turns = int(content_len/step)
+
+        for _ in trange(write_turns):
+            target_content = content[copy_side:(copy_side+step)]
+            time.sleep(0.01)
+            self.driver.imu_write_block(step, copy_side, target_content)
+            time.sleep(0.01)
+            copy_side += step
+            current_side += step
+
+        extract_content_flag = content_len - (step * write_turns)
+        extract_content = content[copy_side:(copy_side+extract_content_flag)]
+        self.driver.imu_write_block(extract_content_flag, copy_side, extract_content)
+        print('imu upgrade finished\n')
+
+    def sdk_part_upgrade(self, content):
+        content_len = len(content)
+        bin_info_list = self.driver.get_bin_info_list(content_len, content)
+        if self.driver.sdk_sync() == False:
+            print('sdk sync failed')
+            self.driver.kill_app(1, 2)
+        # self.driver.flash_write_pre(content)
+        if self.driver.change_buad() == False:
+            print('Prepare baudrate change command failed\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.send_baud(230400) == False:
+            print('Send baudrate command failed\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.baud_check() == False:
+            print('Baudrate check failed\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.is_host_ready() == False:
+            print('Host is not ready.\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.send_boot() == False:
+            print('SDK boot failed\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.send_write_flash() == False:
+            print('Prepare flash change command failed\n')
+            self.driver.kill_app(1, 2)
+        if self.driver.send_bin_info(bin_info_list) == False:
+            print('Send binary info failed')
+            self.driver.kill_app(1, 2)
+        for i in range(2):
+            result = self.driver.wait()
+            if i == 0 and result == False:
+                print('Wait devinit failed')
+            elif i == 1 and result == False:
+                print('Wait erase failed')
+
+        if self.driver.flash_write(content_len, content) == False:
+            print('Write app bin failed')
+            self.driver.kill_app(1, 2)
+
+        for i in range(3):
+            time.sleep(1)
+            result = self.driver.flash_crc()
+            if not result and i == 2:
+                print('CRC check fail')
+            else:
+                break
+
+a = Executor()
+a.upgrade_work()
+    
