09d6c1197f8950e2ef6b19f9aa320261202dced9
darkflamemaster <376346470@qq.com>
2022-04-13 11:09:37 +0800

    DEMO updating

--- main.py
+++ main.py
@@ -11,52 +11,52 @@ from workspace.ethernet.data_center.data_visual import  run_example, my_ex
 data_rev = DataCaptor()
 upgrade = Upgrade_Center()
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # fw_path = './bin/INS401_28.02.01.bin'
     # upgrade = Upgrade_Center()
     # upgrade.upgrade_start(fw_path)
    
     # data_rev.read_data()
-    run_example()
+    # run_example()
 
 
-# def get_cpu_info():
-#     cpu = "%0.2f" % psutil.cpu_percent(interval=1)
-#     data_list.append(float(cpu))
-#     # print(float(cpu))
-#     plot.setData(data_list, pen='g')
+def get_cpu_info():
+    cpu = "%0.2f" % psutil.cpu_percent(interval=1)
+    data_list.append(float(cpu))
+    # print(float(cpu))
+    plot.setData(data_list, pen='g')
 
-# def get_imu_accels():
-#     accel_x = data_rev.get_imu()[0]
-#     print(accel_x)
-#     data_list.append(float(accel_x))
-#     data_list[:-1] = data_list[1:]
-#     data_list[:-1].append(float(accel_x))
+def get_imu_accels():
+    accel_x = data_rev.get_imu()[0]
+    print(accel_x)
+    data_list.append(float(accel_x))
+    data_list[:-1] = data_list[1:]
+    data_list[:-1].append(float(accel_x))
 
-#     # historyLength += 1
-#     curve.setData(data_list, pen='r')
+    # historyLength += 1
+    curve.setData(data_list, pen='r')
 
-# if __name__ == '__main__':
-#     data_list = []
-#     # captor = DataCaptor()
-#     data_rev.connect()
+if __name__ == '__main__':
+    data_list = []
+    # captor = DataCaptor()
+    data_rev.connect()
 
-#     app = pg.mkQApp()
-#     win = pg.GraphicsWindow()
-#     win.setWindowTitle(u'pyqtgraph updating wave')
-#     win.resize(800, 500)
+    app = pg.mkQApp()
+    win = pg.GraphicsWindow()
+    win.setWindowTitle(u'pyqtgraph updating wave')
+    win.resize(800, 500)
 
-#     historyLength = 1000
-#     p = win.addPlot()
-#     p.showGrid(x=True, y=True)
-#     p.setRange(xRange=[0, historyLength], yRange=[-10, 10], padding=0)
-#     p.setLabel(axis='left', text='Acceleration')
-#     p.setLabel(axis='bottom', text='Time')
-#     p.setTitle('IMU X-axis acceleration')
-#     curve = p.plot()
+    historyLength = 1000
+    p = win.addPlot()
+    p.showGrid(x=True, y=True)
+    p.setRange(xRange=[0, historyLength], yRange=[-10, 10], padding=0)
+    p.setLabel(axis='left', text='Acceleration')
+    p.setLabel(axis='bottom', text='Time')
+    p.setTitle('IMU X-axis acceleration')
+    curve = p.plot()
 
-#     timer = pg.QtCore.QTimer()
-#     timer.timeout.connect(get_imu_accels)
-#     timer.start()
+    timer = pg.QtCore.QTimer()
+    timer.timeout.connect(get_imu_accels)
+    timer.start()
 
-#     app.exec_()
+    app.exec_()
--- workspace/ethernet/data_center/data_visual.py
+++ workspace/ethernet/data_center/data_visual.py
@@ -1,9 +1,17 @@
 import time
+import psutil
 import numpy as np
 import pyqtgraph as pg
-import pyqtgraph.examples as ex
 
-data = np.random.normal(size=(500,500))
-ex.run()
+from .data_captor import DataCaptor
 
+class DataVisual:
+    def __init__(self) -> None:
+        self.data_recv = DataCaptor()
 
+    def get_imu_accel_x(self):
+        accel_x = self.data_recv.get_imu()[0]
+        print(accel_x)
+        self.data_list.append(float(accel_x))
+        self.data_list[:-1] = self.data_list[1:]
+        self.data_list[:-1].append(float(accel_x))
