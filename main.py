daf8cb29f4a709d59cc6da3d0f6d1ac5b0e326af
darkflamemaster <376346470@qq.com>
2022-04-15 11:01:17 +0800

    add some test demo

--- main.py
+++ main.py
@@ -1,68 +1,69 @@
-from numpy import true_divide
+import time
+import threading
+import numpy as np
 import pyqtgraph as pg
-import pyqtgraph.examples as examples
+import pyqtgraph.examples as example
 import psutil
 
 from operator import imul
 from sre_constants import CH_LOCALE
+from workspace.communicator.print_center import error_print, pass_print
+from workspace.ethernet.upgrade_center import UpgradeDriver
 from workspace.ethernet.upgrade_center.upgrade_executor import Upgrade_Center
 from workspace.ethernet.data_center.data_captor import DataCaptor
+from workspace.ethernet.data_center.data_visual import IMUDataVisual
 
 
 data_rev = DataCaptor()
 upgrade = Upgrade_Center()
+driver = UpgradeDriver()
+visual = IMUDataVisual()
 
 def show_data():
     data_rev.connect()
-    data = data_rev.read_data()[0]
+    while True:
+        data = data_rev.read_data()[0]
+        print(data)
 
 def upgrade_work():
-    fw_path = './bin/INS401_28.02.01.bin'
+    fw_path = './bin/INS401_v28.03.11.bin'
     upgrade = Upgrade_Center()
     upgrade.upgrade_start(fw_path)
 
-def get_cpu_info():
-    cpu = "%0.2f" % psutil.cpu_percent(interval=1)
-    data_list.append(float(cpu))
-    # print(float(cpu))
-    curve.setData(data_list, pen='g')
+class PingTest:
+    def __init__(self) -> None:
+        pass
 
-def get_imu_accels():
-    accel_x = data_rev.get_imu()[0]
-    print(accel_x)
-    data_list.append(float(accel_x))
-    data_list[:-1] = data_list[1:]
-    data_list[:-1].append(float(accel_x))
+    def clock(self):
+        start_time = time.time()
+        while True:
+            run_time = time.time() - start_time
+            if run_time >= 0.2 and result == False:
+                error_print('200ms ping test failed')
+                driver.kill_app(1, 2)
+            elif run_time >= 0.2 and result == True:
+                pass_print('200ms ping test success')
+                driver.kill_app(1, 2)
 
-    # historyLength += 1
-    curve.setData(data_list, pen='r')
+    def connect(self):
+        global result
+        result = data_rev.connect()
 
-def curve_runer():
-    global curve, data_list
+    def start(self):
+        global result
+        result = False
+        thread_clock = threading.Thread(target=self.clock)
+        thread_connect = threading.Thread(target=self.connect)
 
-    data_list = []
-    data_rev.connect()
+        threads = []
+        threads.append(thread_clock)
+        threads.append(thread_connect)
 
-    app = pg.mkQApp()
-    win = pg.GraphicsWindow()
-    win.setWindowTitle(u'pyqtgraph updating wave')
-    win.resize(800, 500)
-
-    historyLength = 1000
-    p = win.addPlot()
-    p.showGrid(x=True, y=True)
-    p.setRange(xRange=[0, historyLength], yRange=[-10, 10], padding=0)
-    p.setLabel(axis='left', text='Acceleration')
-    p.setLabel(axis='bottom', text='Time')
-    p.setTitle('IMU X-axis acceleration')
-    curve = p.plot()
-
-    timer = pg.QtCore.QTimer()
-    timer.timeout.connect(get_imu_accels)
-    timer.start()
-
-    app.exec_()
-
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
 
 if __name__ == '__main__':
-    curve_runer()
+    visual.curve_runer()
+    # example.run()
