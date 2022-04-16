d7cb73e4741dd9d18ab9694ef4768f08511e6f0d
darkflamemaster <376346470@qq.com>
2022-04-16 10:15:53 +0800

    fixed some output bugs and add the gyro plotting

--- workspace/ethernet/data_center/data_visual.py
+++ workspace/ethernet/data_center/data_visual.py
@@ -23,7 +23,7 @@ class IMUDataVisual:
         accel_y = self.data_recv.get_imu()[2][1]
         # print(accel_y)
         accel_y_lst.append(float(accel_y))
-        accel_y_lst[:-1] = accel_x_lst[1:]
+        accel_y_lst[:-1] = accel_y_lst[1:]
         accel_y_lst[:-1].append(float(accel_y))
 
         curve_accel_y.setData(accel_y_lst, pen='y')
@@ -35,7 +35,7 @@ class IMUDataVisual:
         accel_z_lst[:-1] = accel_z_lst[1:]
         accel_z_lst[:-1].append(float(accel_z))
 
-        curve_accel_z.setData(accel_x_lst, pen='b')
+        curve_accel_z.setData(accel_z_lst, pen='b')
 
     def get_imu_gyro_x(self):
         gyro_x = self.data_recv.get_imu()[3][0]
@@ -59,6 +59,7 @@ class IMUDataVisual:
 
     def curve_runer(self):
         global curve_accel_x, curve_accel_y, curve_accel_z
+        global curve_gyro_x, curve_gyro_y, curve_gyro_z
         global accel_x_lst, accel_y_lst, accel_z_lst
         global gyro_x_lst, gyro_y_lst, gyro_z_lst
 
@@ -74,7 +75,7 @@ class IMUDataVisual:
         win = pg.GraphicsWindow()
         win.setWindowTitle(u'pyqtgraph updating wave')
         win.resize(1000, 800)
-        pg.setConfigOptions(antialias=True)
+        # pg.setConfigOptions(antialias=True)
 
         # plotting for X-axis acceleration
         historyLength = 500
@@ -87,7 +88,6 @@ class IMUDataVisual:
         curve_accel_x = p1.plot()
 
         timer = pg.QtCore.QTimer()
-
         timer.timeout.connect(self.get_imu_accel_x)
         timer.start()
 
@@ -117,8 +117,43 @@ class IMUDataVisual:
         timer.timeout.connect(self.get_imu_accel_z)
         timer.start()
 
+        # switch the plotting row
         win.nextRow()
 
-        p4 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
+        # plotting for X-axis gyro
+        p4 = win.addPlot()
+        p4.showGrid(x=True, y=True)
+        p4.setRange(xRange=[0, historyLength], yRange=[-15, 15], padding=0)
+        p4.setLabel(axis='left', text='Gyro')
+        p4.setLabel(axis='bottom', text='Time')
+        p4.setTitle('IMU X-axis Gyro')
+        curve_gyro_x = p4.plot()
+        
+        timer.timeout.connect(self.get_imu_gyro_x)
+        timer.start()
+
+        # plotting for Y-axis gyro
+        p5 = win.addPlot()
+        p5.showGrid(x=True, y=True)
+        p5.setRange(xRange=[0, historyLength], yRange=[-15, 15], padding=0)
+        p5.setLabel(axis='left', text='Gyro')
+        p5.setLabel(axis='bottom', text='Time')
+        p5.setTitle('IMU Y-axis Gyro')
+        curve_gyro_y = p5.plot()
+
+        timer.timeout.connect(self.get_imu_gyro_y)
+        timer.start()
+
+        # plotting for Z-axis gyro
+        p6 = win.addPlot()
+        p6.showGrid(x=True, y=True)
+        p6.setRange(xRange=[0, historyLength], yRange=[-15, 15], padding=0)
+        p6.setLabel(axis='left', text='Gyro')
+        p6.setLabel(axis='bottom', text='Time')
+        p6.setTitle('IMU Z-axis Gyro')
+        curve_gyro_z = p6.plot()
+
+        timer.timeout.connect(self.get_imu_gyro_z)
+        timer.start()
 
         app.exec_()
