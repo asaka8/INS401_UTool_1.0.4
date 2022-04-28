2e23c3779c1a5259e343a157daf67b5d92297a81
asaka8 <376346470@qq.com>
2022-04-28 09:46:35 +0800

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from concurrent.futures import thread
+import os
 import sys
 import time
 import threading
@@ -8,16 +10,23 @@ import psutil
 
 from operator import imul
 from sre_constants import CH_LOCALE
-from workspace.communicator.print_center import error_print, pass_print
-from workspace.ethernet.upgrade_center import UpgradeDriver
-from workspace.ethernet.upgrade_center.upgrade_executor import Upgrade_Center
-from workspace.ethernet.data_center.data_captor import DataCaptor
-from workspace.ethernet.data_center.data_visual import IMUDataVisual
+from src.communicator.print_center import error_print, pass_print
+from src.ethernet.upgrade_center import UpgradeDriver
+from src.ethernet.upgrade_center.upgrade_executor import Upgrade_Center
+from src.ethernet.data_center.data_captor import DataCaptor
+from src.ethernet.data_center.data_visual import IMUDataVisual
 
-from workspace.ethernet.data_center.data_logger import DataLogger
+from src.ethernet.data_center.data_logger import DataLogger
+from src.communicator.print_center import error_print, pass_print
 
+from src.ethernet.upgrade_center.upgrade_driver import UpgradeDriver
 
-data_rev = DataCaptor()
+# try:
+#     import RPi.GPIO as GPIO
+# except RuntimeError:
+#     print("Error import RPi.GPIO!")
+
+data_recv = DataCaptor()
 upgrade = Upgrade_Center()
 driver = UpgradeDriver()
 visual = IMUDataVisual()
@@ -26,13 +35,13 @@ logger = DataLogger()
 sys.dont_write_bytecode = True
 
 def show_data():
-    data_rev.connect()
+    data_recv.connect()
     while True:
-        data = data_rev.read_data()[0]
+        data = data_recv.read_data()[0]
         print(data)
 
 def upgrade_work():
-    fw_path = './bin/INS401_28.03a.bin'
+    fw_path = './bin/INS401_v28.03.11.bin'
     upgrade = Upgrade_Center()
     upgrade.upgrade_start(fw_path)
 
@@ -53,7 +62,7 @@ class PingTest:
 
     def connect(self):
         global result
-        result = data_rev.connect()
+        result = data_recv.connect()
 
     def start(self):
         global result
@@ -70,8 +79,38 @@ class PingTest:
         for t in threads:
             t.join()
 
+class sample_time:
+    def __init__(self) -> None:
+        self.log_f = open('./timestamp_log.log', 'a+')
+        pass
+
+    def get_pps_time(self):
+        GPIO.setmode(GPIO)
+        GPIO.setup(self.interrupt_pin,GPIO.IN)
+        time.sleep(0.4)
+        GPIO.add_event_detect(self.interrupt_pin,GPIO.RISING) 
+        self.pps_pin_setup()
+        
+        if GPIO.event_detected(self.interrupt_pin):
+            time_stamp = time.time()
+            pass_print(f'\npps time: {time_stamp}\n')
+
+    def first_packet_recv_time(self):
+        data_recv.check_packet_type()
+        return
+
+    def start(self):
+        threads = []
+        pps_thread = threading.Thread(target=self.get_pps_time)
+        get_packet_thrad = threading.Thread(target=self.first_packet_recv_time)
+        threads.append(pps_thread)
+        threads.append(get_packet_thrad)
+
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
 if __name__ == '__main__':
-    # visual.accels_curve_runner()
-    # upgrade_work()
-    # logger.start_log()
-    data_rev.log_raw_data()
+    upgrade_work()
+
